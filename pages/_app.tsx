ffda56d083b29564e59809122c4167f43f30095b
Benjamin Luo <issv374@nus.edu.sg>
2023-05-11 17:43:24 +0800

    Set up MapBox API for the map

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from 'next/app'
 import { ThemeProvider } from 'next-themes'
 import Meta from '@/components/meta'
 import '@/styles/globals.css'
+import '@/styles/maps.css';
 
 const App = ({ Component, pageProps }: AppProps) => {
 	return (
