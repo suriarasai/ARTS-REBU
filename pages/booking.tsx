852d256da98a847522d705f606888775c3cdf7c0
Benjamin Luo <issv374@nus.edu.sg>
2023-05-12 13:29:08 +0800

    Added basic routing between 2 points (very messy, copied from documentation but functional

--- pages/booking.tsx
+++ pages/booking.tsx
@@ -6,7 +6,7 @@ import React, { useRef, useEffect, useState } from 'react'
 import Page from '@/components/page'
 import Section from '@/components/section'
 import mapboxgl from 'mapbox-gl'
-import { AddressAutofill, SearchBox } from '@mapbox/search-js-react'
+import { SearchBox } from '@mapbox/search-js-react'
 import SearchLocations from './searchLocations'
 
 mapboxgl.accessToken =
@@ -28,14 +28,145 @@ const Booking = () => {
 
 		// Centers map on current location
 		navigator.geolocation.getCurrentPosition((position) => {
-			map.current = new mapboxgl.Map({
-				container: mapContainer.current,
-				style: 'mapbox://styles/mapbox/light-v11',
-				center: [position.coords.longitude, position.coords.latitude],
-				zoom: zoom,
-			})
+			setLng(position.coords.longitude)
+			setLat(position.coords.latitude)
 		})
-	
+
+		// Creates the map object
+		map.current = new mapboxgl.Map({
+			container: mapContainer.current,
+			style: 'mapbox://styles/mapbox/light-v11',
+			center: [lng, lat],
+			zoom: zoom,
+		})
+	})
+
+	const start = [103.773869, 1.299151]
+
+	// create a function to make a directions request
+	async function getRoute(end) {
+		// make a directions request using cycling profile
+		// an arbitrary start will always be the same
+		// only the end or destination will change
+		const query = await fetch(
+			`https://api.mapbox.com/directions/v5/mapbox/cycling/${start[0]},${start[1]};${end[0]},${end[1]}?steps=true&geometries=geojson&access_token=${mapboxgl.accessToken}`,
+			{ method: 'GET' }
+		)
+		const json = await query.json()
+		const data = json.routes[0]
+		const route = data.geometry.coordinates
+		const geojson = {
+			type: 'Feature',
+			properties: {},
+			geometry: {
+				type: 'LineString',
+				coordinates: route,
+			},
+		}
+		// if the route already exists on the map, we'll reset it using setData
+		if (map.current?.getSource('route')) {
+			map.current?.getSource('route').setData(geojson)
+		}
+		// otherwise, we'll make a new request
+		else {
+			map.current?.addLayer({
+				id: 'route',
+				type: 'line',
+				source: {
+					type: 'geojson',
+					data: geojson,
+				},
+				layout: {
+					'line-join': 'round',
+					'line-cap': 'round',
+				},
+				paint: {
+					'line-color': '#3887be',
+					'line-width': 5,
+					'line-opacity': 0.75,
+				},
+			})
+		}
+		// add turn instructions here at the end
+	}
+
+	map.current?.on('load', () => {
+		// make an initial directions request that
+		// starts and ends at the same location
+		getRoute(start)
+
+		// Add starting point to the map
+		map.current?.addLayer({
+			id: 'point',
+			type: 'circle',
+			source: {
+				type: 'geojson',
+				data: {
+					type: 'FeatureCollection',
+					features: [
+						{
+							type: 'Feature',
+							properties: {},
+							geometry: {
+								type: 'Point',
+								coordinates: start,
+							},
+						},
+					],
+				},
+			},
+			paint: {
+				'circle-radius': 10,
+				'circle-color': '#3887be',
+			},
+		})
+		
+		// Adds a marker where the user clicks on the map
+		map.current?.on('click', (event) => {
+			const coords = Object.keys(event.lngLat).map((key) => event.lngLat[key])
+			const end = {
+				type: 'FeatureCollection',
+				features: [
+					{
+						type: 'Feature',
+						properties: {},
+						geometry: {
+							type: 'Point',
+							coordinates: coords,
+						},
+					},
+				],
+			}
+			if (map.current?.getLayer('end')) {
+				map.current?.getSource('end').setData(end)
+			} else {
+				map.current?.addLayer({
+					id: 'end',
+					type: 'circle',
+					source: {
+						type: 'geojson',
+						data: {
+							type: 'FeatureCollection',
+							features: [
+								{
+									type: 'Feature',
+									properties: {},
+									geometry: {
+										type: 'Point',
+										coordinates: coords,
+									},
+								},
+							],
+						},
+					},
+					paint: {
+						'circle-radius': 10,
+						'circle-color': '#f30',
+					},
+				})
+			}
+			getRoute(coords)
+		})
 	})
 
 	return (
