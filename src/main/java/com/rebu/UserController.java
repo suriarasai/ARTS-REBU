a448745ae2b66397803561a54456739b0bf1c067
Benjamin Luo <issv374@nus.edu.sg>
2023-05-17 14:30:16 +0800

    Added cross origin permissions

--- src/main/java/com/rebu/UserController.java
+++ src/main/java/com/rebu/UserController.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,6 +19,7 @@ import com.rebu.data.User;
 
 @RestController
 @RequestMapping("/api/v1/customers")
+@CrossOrigin(origins="*")
 public class UserController {
     @Autowired
     private UserService userService;
