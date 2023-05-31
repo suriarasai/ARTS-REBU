1b98aceec4213dafc6abffaf3b4e4e85383d95d7
Benjamin Luo <issv374@nus.edu.sg>
2023-05-31 15:01:21 +0800

    UI improvements to bottom navBar

--- client/pages/home.tsx
+++ client/pages/home.tsx
@@ -33,98 +33,32 @@ const Home = () => {
 
 			{/* Bottom Visual */}
 			<div className='h-3/5 p-6'>
-				<div className='flex h-full flex-wrap justify-center gap-4 pb-20 pt-6 text-center'>
-					<div className='flex w-1/3 flex-col items-center justify-center'>
-						<div
-							className='main-screen-box flex items-center justify-center'
-							onClick={() => router.push('/booking')}
-						>
-							<svg
-								viewBox='0 0 15 15'
-								fill='none'
-								xmlns='http://www.w3.org/2000/svg'
-								width='30'
-								height='30'
-								color='white'
-							>
-								<path
-									fillRule='evenodd'
-									clipRule='evenodd'
-									d='M2.197 2.698A3.5 3.5 0 015.198 1h4.604a3.5 3.5 0 013 1.698L15 6.358V12.5a1.5 1.5 0 01-1.5 1.5h-1a1.5 1.5 0 01-1.5-1.5V12H4v.5A1.5 1.5 0 012.5 14h-1A1.5 1.5 0 010 12.5V6.358l2.197-3.66zM12 7H3V6h9v1zM2 10h3V9H2v1zm11-1h-3v1h3V9z'
-									fill='currentColor'
-								></path>
-							</svg>
-						</div>
-						<p className='pt-2 text-sm'>Ride</p>
-					</div>
+				<div className='flex h-full flex-wrap justify-center gap-4 pb-20 md:pb-0 lg:pb-0 pt-6 text-center'>
+					
+                    <MainScreenButton 
+                        label='Ride'
+                        icon='M2.197 2.698A3.5 3.5 0 015.198 1h4.604a3.5 3.5 0 013 1.698L15 6.358V12.5a1.5 1.5 0 01-1.5 1.5h-1a1.5 1.5 0 01-1.5-1.5V12H4v.5A1.5 1.5 0 012.5 14h-1A1.5 1.5 0 010 12.5V6.358l2.197-3.66zM12 7H3V6h9v1zM2 10h3V9H2v1zm11-1h-3v1h3V9z'
+                        href='/booking'
+                    />
+						
 
-					<div className='flex w-1/3 flex-col items-center justify-center'>
-						<div
-							className='main-screen-box flex items-center justify-center'
-							onClick={() => router.push('/booking')}
-						>
-							<svg
-								viewBox='0 0 15 15'
-								fill='none'
-								xmlns='http://www.w3.org/2000/svg'
-								width='30'
-								height='30'
-								color='white'
-							>
-								<path
-									d='M13 0H2v14.5a.5.5 0 00.812.39L7.5 11.14l4.688 3.75A.5.5 0 0013 14.5V0z'
-									fill='currentColor'
-								></path>
-							</svg>
-						</div>
-						<p className='pt-2 text-sm'>Locations</p>
-					</div>
+					<MainScreenButton
+						label='Locations'
+						icon='M13 0H2v14.5a.5.5 0 00.812.39L7.5 11.14l4.688 3.75A.5.5 0 0013 14.5V0z'
+						href='/savedPlaces'
+					/>
 
-					<div className='flex w-1/3 flex-col items-center justify-center'>
-						<div
-							className='main-screen-box flex items-center justify-center'
-							onClick={() => router.push('/booking')}
-						>
-							<svg
-								viewBox='0 0 15 15'
-								fill='none'
-								xmlns='http://www.w3.org/2000/svg'
-								width='30'
-								height='30'
-								color='white'
-							>
-								<path
-									fillRule='evenodd'
-									clipRule='evenodd'
-									d='M0 1.5C0 .67.671 0 1.5 0h12c.829 0 1.5.67 1.5 1.5v8.993c0 .83-.671 1.5-1.5 1.5H7.667L3.8 14.89a.5.5 0 01-.8-.4v-2.498H1.5c-.829 0-1.5-.67-1.5-1.5V1.5zm4 2.497h7v1H4v-1zm0 2.998h5v1H4v-1z'
-									fill='currentColor'
-								></path>
-							</svg>
-						</div>
-						<p className='pt-2 text-sm'>Messages</p>
-					</div>
+					<MainScreenButton
+						label='Messages'
+						icon='M0 1.5C0 .67.671 0 1.5 0h12c.829 0 1.5.67 1.5 1.5v8.993c0 .83-.671 1.5-1.5 1.5H7.667L3.8 14.89a.5.5 0 01-.8-.4v-2.498H1.5c-.829 0-1.5-.67-1.5-1.5V1.5zm4 2.497h7v1H4v-1zm0 2.998h5v1H4v-1z'
+						href='/settings'
+					/>
 
-					<div className='flex w-1/3 flex-col items-center justify-center'>
-						<div
-							className='main-screen-box flex items-center justify-center'
-							onClick={() => router.push('/booking')}
-						>
-							<svg
-								viewBox='0 0 15 15'
-								fill='none'
-								xmlns='http://www.w3.org/2000/svg'
-								width='30'
-								height='30'
-                                color='white'
-							>
-								<path
-									d='M9.312 2.995A2.034 2.034 0 005.776.992L3 5.354V12.5A2.5 2.5 0 005.5 15h5a2.5 2.5 0 002-1l2.5-3.333V7.5A2.5 2.5 0 0012.5 5H8.309l1.003-2.005zM0 5v10h1V5H0z'
-									fill='currentColor'
-								></path>
-							</svg>
-						</div>
-						<p className='pt-2 text-sm'>Reviews</p>
-					</div>
+					<MainScreenButton
+						label='Reviews'
+						icon='M9.312 2.995A2.034 2.034 0 005.776.992L3 5.354V12.5A2.5 2.5 0 005.5 15h5a2.5 2.5 0 002-1l2.5-3.333V7.5A2.5 2.5 0 0012.5 5H8.309l1.003-2.005zM0 5v10h1V5H0z'
+						href='/settings'
+					/>
 				</div>
 			</div>
 
@@ -132,6 +66,36 @@ const Home = () => {
 			<div className='sm:hidden'>
 				<BottomNav />
 			</div>
+		</div>
+	)
+}
+
+const MainScreenButton = ({ label, icon, href }) => {
+	const router = useRouter()
+
+	return (
+		<div className='flex w-1/3 flex-col items-center justify-center'>
+			<div
+				className='main-screen-box flex items-center justify-center'
+				onClick={() => router.push(href)}
+			>
+				<svg
+					viewBox='0 0 15 15'
+					fill='none'
+					xmlns='http://www.w3.org/2000/svg'
+					width='30'
+					height='30'
+					color='white'
+				>
+					<path
+						fillRule='evenodd'
+						clipRule='evenodd'
+						d={icon}
+						fill='currentColor'
+					></path>
+				</svg>
+			</div>
+			<p className='pt-2 text-sm'>{label}</p>
 		</div>
 	)
 }
