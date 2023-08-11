89c8154e9d955a6f6912afd728f9a5bd0afe760d
Benjamin Luo <issv374@nus.edu.sg>
2023-08-11 16:31:55 +0800

    Additional styling

--- customer-app/components/Map/TripScreens/Selection/routeDetails.tsx
+++ customer-app/components/Map/TripScreens/Selection/routeDetails.tsx
@@ -1,6 +1,5 @@
-import { originAtom, destinationAtom, userLocationAtom } from '@/state';
-import { useRecoilValue } from 'recoil';
-
+import { originAtom, destinationAtom, userLocationAtom } from '@/state'
+import { useRecoilValue } from 'recoil'
 
 export function RouteDetails() {
 	const origin = useRecoilValue(originAtom)
@@ -8,7 +7,7 @@ export function RouteDetails() {
 	const userLocation = useRecoilValue(userLocationAtom)
 
 	return (
-		<div className='absolute left-0 right-0 top-0 ml-auto mr-auto mt-20 w-5/6 max-w-screen-md space-y-4 rounded-lg bg-gray-700 p-4'>
+		<div className='responsive absolute left-0 right-0 top-0 w-5/6 space-y-4 rounded-b-lg bg-gray-700 p-4'>
 			<div className='flex items-center rounded-sm bg-gray-600 text-sm text-zinc-100'>
 				<label className='my-0 mr-3 w-12 bg-gray-500 p-2 text-zinc-100'>
 					From
